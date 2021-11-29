504029aa131b0248cc76bf1538e2b462b8c8ab54
920DEV <93388817+920DEV@users.noreply.github.com>
2021-11-29 15:46:26 +0530

    Now we Can Also play snake water gun with computers too.

--- game.py
+++ game.py
@@ -12,39 +12,47 @@ while No_of_chances<chance:          # If the" no_of_chances" will exceeds the "
     Player_input= input("Snake(s), Water(w), Gun(g):")
     computer_input=random.choice(user_options)
     # If the user and computer input the same option then that game will be tie.
-    if user_options==computer_input:
+    if Player_input==computer_input:
         print("This Round is tied")
+        print(f"Player choose:{Player_input} and Computer choose: {computer_input}")
     # when Player choose snake as option:
     elif Player_input=="s":
         if computer_input=="w":
             player_points=player_points+1
+            print(f"Player choose:{Player_input} and Computer choose: {computer_input}")
             print("You win this round:-")
         elif computer_input=="g":
             computer_points=computer_points+1
+            print(f"Player choose:{Player_input} and Computer choose: {computer_input}")
             print("You Loose this round:!")
     # when player choose gun as option:
     elif Player_input=="g":
         if computer_input=="w":
             computer_points=computer_points+1
-            print("You Lose")
+            print(f"Player choose:{Player_input} and Computer choose: {computer_input}")
+            print("You Lose this round")
         elif computer_input=="s":
             player_points=player_points+1
-            print("you won:-")
+            print(f"Player choose:{Player_input} and Computer choose: {computer_input}")
+            print("you won this round:-")
     # when player choose water as option:
     elif Player_input=="w":
         if computer_input=="s":
             player_points=player_points+1
-            print("You won :-")
+            print(f"Player choose:{Player_input} and Computer choose: {computer_input}")
+            print("You won this round :-")
         elif computer_input=="g":
             computer_points=computer_points+1
-            print("You Loose ")
+            print(f"Player choose:{Player_input} and Computer choose: {computer_input}")
+            print("You Loose this round")
     # when player choose put a wrong input
     else:
         print("you  put a wrong input:!")
 
     No_of_chances=No_of_chances+1
     print(f"{chance-No_of_chances} chances left")
-    print("Game Over.")
+
+print("Game Over.")
 print(f"computer get {computer_points} Points.")
 print(f"Player get {player_points} Points.")
 if computer_points>player_points:
